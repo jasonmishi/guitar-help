0d2ceb75760e18708eb99d586bdf73fde65138c5
Jason Mishike <jasonmishi@gmail.com>
2023-05-06 18:08:15 +0530

    formatting

--- src/ChordChart.tsx
+++ src/ChordChart.tsx
@@ -1,26 +1,26 @@
 import { useEffect } from 'react';
 import { SVGuitarChord, Chord, } from 'svguitar'
 
-function ChordChart({chord} : {chord: Chord}) {
-    useEffect(() => {
-        const chart = new SVGuitarChord('.chord-chart')
-        .chord(
-          chord
-        )
-        .configure({
-          color: '#888', 
-        })
-  
-        chart.draw();
-  
-        return () => chart.remove();
-    })
+function ChordChart({ chord }: { chord: Chord }) {
+  useEffect(() => {
+    const chart = new SVGuitarChord('.chord-chart')
+      .chord(
+        chord
+      )
+      .configure({
+        color: '#888',
+      })
+
+    chart.draw();
+
+    return () => chart.remove();
+  })
 
   return (
     <>
       <div className="chord-chart"></div>
     </>
-    )
+  )
 }
 
 export default ChordChart;
--- src/FretboardMemorizationExercise.tsx
+++ src/FretboardMemorizationExercise.tsx
@@ -5,7 +5,7 @@ interface FretboardMemorizationExerciseProps {
   notesInOrder: string[]
 }
 
-function FretboardMemorizationExercise({notesInOrder}: FretboardMemorizationExerciseProps) {
+function FretboardMemorizationExercise({ notesInOrder }: FretboardMemorizationExerciseProps) {
   const [randomStrings, setRandomStrings] = useState(["1", "2", "3", "4", "5", "6"].sort(() => Math.random() - 0.5))
   const randomizeStrings = () => {
     setRandomStrings([...randomStrings.sort(() => Math.random() - 0.5)])
@@ -13,19 +13,19 @@ function FretboardMemorizationExercise({notesInOrder}: FretboardMemorizationExer
 
   const notesComponents: ReactElement[] = []
   notesInOrder.forEach((note) => {
-    notesComponents.push(<Note key={ note } value = { note } parentOnClick = { randomizeStrings } />)
+    notesComponents.push(<Note key={note} value={note} parentOnClick={randomizeStrings} />)
   })
 
   return (
     <>
       <h1>
-        { notesComponents }
+        {notesComponents}
       </h1>
       <h2>
-        { randomStrings.map((string) => (" " + string)) }
+        {randomStrings.map((string) => (" " + string))}
       </h2>
     </>
-    )
+  )
 }
 
 export default FretboardMemorizationExercise
