923198271e7bc106d8d05fd581627e6eb6d8a038
Jason Mishike <jasonmishi@gmail.com>
2023-05-12 22:01:36 +0530

    formatting

--- src/components/ChordChart.tsx
+++ src/components/ChordChart.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from 'react';
-import { SVGuitarChord, Shape } from 'svguitar'
-import { Chord as SVGChord } from 'svguitar';
+import { SVGuitarChord, Shape, Chord as SVGChord } from 'svguitar'
 
 function chordToSVGuitarChord(chord: Chord): SVGChord {
   let svgChord: SVGChord = {
