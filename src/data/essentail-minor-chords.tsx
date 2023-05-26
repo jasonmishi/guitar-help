fef0c71adbeef5becd689dca6d14fc25ea27b055
Jason Mishike <jasonmishi@gmail.com>
2023-05-26 21:57:56 +0530

    initial commit

---
+++ src/data/essentail-minor-chords.tsx
@@ -0,0 +1,17 @@
+interface Strings {
+  "1": Chord[],
+  "2": Chord[],
+  "3": Chord[],
+  "4": Chord[],
+  "5": Chord[],
+  "6": Chord[],
+}
+
+export const essentailMajorChords: Record<keyof Strings, Chord[]> = {
+  1: [],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+  6: []
+} 
