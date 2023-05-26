a63a53a968f576ae072ba0c319cefc35e805d2ec
Jason Mishike <jasonmishi@gmail.com>
2023-05-26 22:14:18 +0530

    add onChange function to chord type dropdown

--- src/components/ChordTypeSelect.tsx
+++ src/components/ChordTypeSelect.tsx
@@ -5,7 +5,7 @@ interface ChordTypeSelectProps {
 function ChordTypeSelect({onChange}: ChordTypeSelectProps) {
 
   return (
-    <select name="chord-type" id="chord-type">
+    <select name="chord-type" id="chord-type" onChange={onChange}>
       <option value="major">Major</option>
       <option value="minor">Minor</option>
     </select>
