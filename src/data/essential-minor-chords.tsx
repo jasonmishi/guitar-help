9130a070cc7d2a7b3984cdab95f7de8f46e62a19
Jason Mishike <jasonmishi@gmail.com>
2023-05-31 22:00:25 +0530

    add first string root minor chords

--- src/data/essential-minor-chords.tsx
+++ src/data/essential-minor-chords.tsx
@@ -8,7 +8,52 @@ interface Strings {
 }
 
 export const essentialMinorChords: Record<keyof Strings, Chord[]> = {
-  1: [],
+  1: [
+    {
+      // array of [string, fret, text | options]
+      fingers: [
+        [1, 4],
+        [2, 4, { optional: true }],
+        [3, 4],
+        [4, 1],
+        [5, 2, { optional: true }],
+        [6, 4, { optional: true }],
+      ],
+      barres: []
+    },
+    {
+      fingers: [
+        [1, 3],
+        [2, 3],
+        [3, 3, { optional: true }],
+        [5, 1],
+      ],
+      barres: []
+    },
+    {
+      fingers: [
+        [1, 1],
+        [2, 1],
+        [3, 1],
+        [4, 3, { optional: true }],
+        [5, 3, { optional: true }],
+        [6, 1, { optional: true }],
+        [6, 4, { optional: true }],
+      ],
+      barres: []
+    },
+    {
+      fingers: [
+        [1, 1],
+        [2, 1, { optional: true }],
+        [3, 1],
+        [4, 3, { optional: true }],
+        [5, 3],
+        [6, 1, { optional: true }],
+      ],
+      barres: []
+    },
+  ],
   2: [],
   3: [],
   4: [],
